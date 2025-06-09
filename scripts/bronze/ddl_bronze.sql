f057ee08de075fbc0face0c8350f92a09e9a1f96
rajc6h6 <154867531+rajc6h6@users.noreply.github.com>
2025-06-09 05:55:39 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,1 +1,9 @@
+BULK INSERT bronze.supermarket_data
+FROM 'F:\sql material\archive\Sample - Superstore.csv'
+WITH (
+    FIRSTROW = 2,               -- skip header row
+    FIELDTERMINATOR = ',',      -- CSV comma separator
+    ROWTERMINATOR = '\n',       -- line break as row separator
+    TABLOCK                    -- optimize for bulk insert
+);
 

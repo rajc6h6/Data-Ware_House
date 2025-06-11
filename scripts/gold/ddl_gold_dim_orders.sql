e409c109a859e4a99b2a8d7dc9db83865e3004a4
rajc6h6 <154867531+rajc6h6@users.noreply.github.com>
2025-06-11 23:51:05 +0530

    Update ddl_gold_dim_orders.sql

--- scripts/gold/ddl_gold_dim_orders.sql
+++ scripts/gold/ddl_gold_dim_orders.sql
@@ -1,1 +1,10 @@
+-- Gold Layer: Dimension Table - Orders
 
+CREATE OR ALTER VIEW gold.dim_orders AS
+SELECT DISTINCT
+    order_id,
+    order_date,
+    ship_date,
+    ship_mode,
+    customer_id
+FROM silver.orders;

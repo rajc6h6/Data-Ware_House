db17c5e58b2b01c7a6a61b114271d56550f6e65f
rajc6h6 <154867531+rajc6h6@users.noreply.github.com>
2025-06-11 23:54:41 +0530

    Update ddl_gold.dim_products.sql

--- scripts/gold/ddl_gold.dim_products.sql
+++ scripts/gold/ddl_gold.dim_products.sql
@@ -1,1 +1,11 @@
+-- Gold Layer: Dimension Table - Products
+
+CREATE OR ALTER VIEW gold.dim_products AS
+SELECT DISTINCT
+    product_id,
+    product_names,
+    category,
+    sub_category
+FROM silver.products;
+
 
